60d3fee688788971f211f89ac38b9c3c461c3424
Ahmed Gaballah <48418000+gaballah11@users.noreply.github.com>
2023-08-24 03:09:45 +0300

    Update scoring.dart

--- lib/modules/scoring.dart
+++ lib/modules/scoring.dart
@@ -2,28 +2,28 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class scoring {
   late int dashWinUnder;
-  late  int dashWinOver ;
-  late  int dashLoseUnder ;
-  late int dashLoseOver ;
-  late  int win ;
-  late  int lose ;
-  late  int onlyWin;
-  late  int onlyLose;
+  late int dashWinOver;
+  late int dashLoseUnder;
+  late int dashLoseOver;
+  late int win;
+  late int lose;
+  late int onlyWin;
+  late int onlyLose;
 
-  late  bool doubleThreeCalls;
+  late bool doubleThreeCalls;
 
-  scoring(){
+  scoring() {
     initializeVarZero();
     print("Scoring constructor");
     getScoringFromDb().then((value) {
-      if(value== false){
+      if (value == false) {
+        print("cococococococococ");
         resetToDefault();
       }
     });
   }
 
-
-  resetToDefault(){
+  resetToDefault() {
     print("Reset to default");
     dashWinUnder = 33;
     dashWinOver = 23;
@@ -31,7 +31,7 @@ class scoring {
     dashLoseOver = 33;
     win = 13;
     lose = 0;
-    onlyWin =10;
+    onlyWin = 10;
     onlyLose = 10;
 
     doubleThreeCalls = true;
@@ -57,7 +57,7 @@ class scoring {
   Future<bool> getScoringFromDb() async {
     print("get from db");
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    if(prefs.containsKey("win")){
+    if (prefs.containsKey("win")) {
       print("true");
       dashWinUnder = prefs.getInt("dashWinUnder")!;
       dashWinOver = prefs.getInt("dashWinOver")!;
@@ -70,25 +70,36 @@ class scoring {
 
       doubleThreeCalls = prefs.getBool("doubleThreeCalls")!;
       return true;
-    }
-    else{
+    } else {
       print(false);
       return false;
     }
-
   }
 
   void initializeVarZero() {
     dashWinUnder = 0;
-    dashWinOver = 0 ;
-    dashLoseUnder = 0 ;
-    dashLoseOver = 0 ;
-    win = 0 ;
-    lose = 0 ;
+    dashWinOver = 0;
+    dashLoseUnder = 0;
+    dashLoseOver = 0;
+    win = 0;
+    lose = 0;
     onlyWin = 0;
     onlyLose = 0;
 
     doubleThreeCalls = false;
   }
 
+  void printing() {
+    print('scoring system :');
+    print(dashLoseOver);
+    print(dashLoseUnder);
+    print(dashWinOver);
+    print(dashWinUnder);
+    print(doubleThreeCalls);
+    print(lose);
+    print(onlyLose);
+    print(onlyWin);
+    print(win);
+    print('_____________________');
+  }
 }
