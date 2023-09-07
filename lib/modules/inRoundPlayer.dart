9e79ef5dd365da10e710a87d72f6c3d36dcf0f72
Ahmed Gaballah <midogaballah14@gmail.com>
2023-09-07 16:54:21 +0300

    v1.1

--- lib/modules/inRoundPlayer.dart
+++
@@ -1,6 +0,0 @@
-import 'package:estimation_calculator/modules/inGamePlayer.dart';
-import 'package:estimation_calculator/modules/player.dart';
-
-class inRoundPlayer extends inGamePlayer {
-  inRoundPlayer(Player p) : super(p);
-}
